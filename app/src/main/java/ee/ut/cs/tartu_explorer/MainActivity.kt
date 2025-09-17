b9fe613021f23b000fe6d6f4127a6d632459f69b
Philipp Rebeschieß <philipp.rebeschiess@uni-potsdam.de>
2025-09-17 18:19:33 +0300

    Initialize Android project with default configuration:
    - Add `build.gradle.kts` and necessary Gradle settings.
    - Configure AndroidManifest with core app setup.
    - Include default UI assets and theme resources.
    - Set up `.gitignore` for Android/IDE-specific files.
    - Add sample test files for unit and instrumentation testing.

---
+++ app/src/main/java/ee/ut/cs/tartu_explorer/MainActivity.kt
@@ -0,0 +1,47 @@
+package ee.ut.cs.tartu_explorer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ee.ut.cs.tartu_explorer.ui.theme.TartuExplorerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TartuExplorerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TartuExplorerTheme {
+        Greeting("Android")
+    }
+}

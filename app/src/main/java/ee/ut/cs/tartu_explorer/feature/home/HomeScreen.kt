2e8879c1ab912be8f9788acddcacdbd28d75ba03
Rando Roosik <roosikrando@gmail.com>
2025-09-25 18:47:33 +0300

    Some additons to UI, mostly worked on Play screen

--- app/src/main/java/ee/ut/cs/tartu_explorer/feature/home/HomeScreen.kt
+++ app/src/main/java/ee/ut/cs/tartu_explorer/feature/home/HomeScreen.kt
@@ -4,11 +4,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(
@@ -17,10 +23,28 @@ fun HomeScreen(
     onNavigateToGame: () -> Unit
 ) {
     Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        // Game Tittle in the middle
+        Text(
+            text = "TARTU EXPLORER",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            lineHeight = 40.sp
+        )
+
+    }
+
+    Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
         Button(onClick = onNavigateToQuest) {
             Text("Quest")
         }
--- app/src/main/java/ee/ut/cs/tartu_explorer/feature/statistics/StatisticsScreen.kt
+++ app/src/main/java/ee/ut/cs/tartu_explorer/feature/statistics/StatisticsScreen.kt
@@ -1,25 +1,61 @@
 package ee.ut.cs.tartu_explorer.feature.statistics
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 //Statistics Screen
 fun StatisticsScreen(onNavigateBack: () -> Unit) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
+            .padding(32.dp),
         contentAlignment = Alignment.Center
     ){
-        Button(onClick = onNavigateBack) {
+        // Back Button to "HomeScreen"
+        Button(
+            onClick = onNavigateBack,
+            modifier = Modifier.align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp)
+        ) {
             Text("Back")
         }
 
         //to Delete
-        Text("This is statistics Screen")
+        Text("This is statistics Screen",
+            textAlign = TextAlign.Start)
+
+        // Bordered box are in the middle
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(600.dp)
+                .border(2.dp, Color.Black)
+                .padding(16.dp)
+
+        ){
+            // "Statistics" title in the bordered box
+            Text(
+                text = "Statistics",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+
+        }
     }
+
 }

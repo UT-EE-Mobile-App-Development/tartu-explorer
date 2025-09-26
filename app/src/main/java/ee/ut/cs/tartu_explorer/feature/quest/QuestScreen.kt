4defe50a01c1eb701b42acd04e5d8d6efe029872
madis <puumadis@gmail.com>
2025-09-26 16:04:49 +0300

    Quest pictures

--- app/src/main/java/ee/ut/cs/tartu_explorer/feature/quest/QuestScreen.kt
+++ app/src/main/java/ee/ut/cs/tartu_explorer/feature/quest/QuestScreen.kt
@@ -1,30 +1,66 @@
 package ee.ut.cs.tartu_explorer.feature.quest
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import ee.ut.cs.tartu_explorer.R
 
 @Composable
 //Quests Screen
 fun QuestScreen(onNavigateBack: () -> Unit) {
+    //to be replaced with a function that gets the data from the backend someday
+    val easyLevelsPictures = listOf(
+        R.drawable.temp_progress_bar_1,
+        R.drawable.temp_progress_bar_2,
+        R.drawable.temp_progress_bar_3,
+        R.drawable.temp_progress_bar_4,
+        R.drawable.temp_progress_bar_5,
+        R.drawable.temp_progress_bar_6
+    )
+
+    //to be replaced with a function that gets the data from the backend someday
+    val mediumLevelsPictures = listOf(
+        R.drawable.temp_progress_bar_1,
+        R.drawable.temp_progress_bar_2,
+        R.drawable.temp_progress_bar_3,
+        R.drawable.temp_progress_bar_4,
+        R.drawable.temp_progress_bar_5,
+        R.drawable.temp_progress_bar_6
+    )
+
+    //to be replaced with a function that gets the data from the backend someday
+    val hardLevelsPictures = listOf(
+        R.drawable.temp_progress_bar_1,
+        R.drawable.temp_progress_bar_2,
+        R.drawable.temp_progress_bar_3,
+        R.drawable.temp_progress_bar_4,
+        R.drawable.temp_progress_bar_5,
+        R.drawable.temp_progress_bar_6
+    )
+
+    //scroll states for the pictures on different difficulties
+    val easyScrollState = rememberScrollState()
+    val mediumScrollState = rememberScrollState()
+    val hardScrollState = rememberScrollState()
+
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-        Button(
-            onClick = onNavigateBack,
-            modifier = Modifier.align(Alignment.BottomCenter)
-                .padding(bottom = 20.dp)
-            ) {
-            Text("Back")
-        }
+
         //to Delete
         Text("This is Quests Screen")
 
@@ -37,15 +73,26 @@ fun QuestScreen(onNavigateBack: () -> Unit) {
                     top = 70.dp
                 )
         ){
+
             //easy difficulty
             Row(){
                 Column(){
                     Text("Easy")
+
                     //pictures for easy difficulty
-                    Row(){
-                        Text("picture")
-                        Text("picture")
-                        Text("picture")
+                    Row(
+                        modifier = Modifier.horizontalScroll(easyScrollState) // enables scrolling
+                    ){
+                        //displays each picture from easyLevelsPictures
+                        easyLevelsPictures.forEach { picture ->
+                            Image(
+                                painter = painterResource(id = picture),
+                                contentDescription = "Easy levels",
+                                modifier = Modifier
+                                    .size(100.dp)
+                                    .padding(4.dp)
+                            )
+                        }
                     }
                 }
             }
@@ -55,10 +102,19 @@ fun QuestScreen(onNavigateBack: () -> Unit) {
                 Column(){
                     Text("Medium")
                     //pictures for medium difficulty
-                    Row(){
-                        Text("picture")
-                        Text("picture")
-                        Text("picture")
+                    Row(
+                        modifier = Modifier.horizontalScroll(mediumScrollState) // enables scrolling
+                    ){
+                        //displays each picture from mediumLevelsPictures
+                        mediumLevelsPictures.forEach { picture ->
+                            Image(
+                                painter = painterResource(id = picture),
+                                contentDescription = "Easy levels",
+                                modifier = Modifier
+                                    .size(100.dp)
+                                    .padding(4.dp)
+                            )
+                        }
                     }
                 }
             }
@@ -68,13 +124,30 @@ fun QuestScreen(onNavigateBack: () -> Unit) {
                 Column(){
                     Text("Hard")
                     //pictures for hard difficulty
-                    Row(){
-                        Text("picture")
-                        Text("picture")
-                        Text("picture")
+                    Row(
+                        modifier = Modifier.horizontalScroll(hardScrollState) // enables scrolling
+                    ){
+                        //displays each picture from hardLevelsPictures
+                        hardLevelsPictures.forEach { picture ->
+                            Image(
+                                painter = painterResource(id = picture),
+                                contentDescription = "Easy levels",
+                                modifier = Modifier
+                                    .size(100.dp)
+                                    .padding(4.dp)
+                            )
+                        }
                     }
                 }
             }
+        }
+
+        Button(
+            onClick = onNavigateBack,
+            modifier = Modifier.align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp)
+        ) {
+            Text("Back")
         }
     }
 

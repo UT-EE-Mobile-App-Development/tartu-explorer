4fbc2b9d13c51068877e31d35fcead222211b3ed
madis <puumadis@gmail.com>
2025-09-26 11:42:47 +0300

    Quests screen blueprint

--- app/src/main/java/ee/ut/cs/tartu_explorer/feature/quest/QuestScreen.kt
+++ app/src/main/java/ee/ut/cs/tartu_explorer/feature/quest/QuestScreen.kt
@@ -1,5 +1,7 @@
 package ee.ut.cs.tartu_explorer.feature.quest
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -25,5 +27,55 @@ fun QuestScreen(onNavigateBack: () -> Unit) {
         }
         //to Delete
         Text("This is Quests Screen")
+
+        //for Quest difficulties
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(
+                    start = 20.dp,
+                    top = 70.dp
+                )
+        ){
+            //easy difficulty
+            Row(){
+                Column(){
+                    Text("Easy")
+                    //pictures for easy difficulty
+                    Row(){
+                        Text("picture")
+                        Text("picture")
+                        Text("picture")
+                    }
+                }
+            }
+
+            //medium difficulty
+            Row(){
+                Column(){
+                    Text("Medium")
+                    //pictures for medium difficulty
+                    Row(){
+                        Text("picture")
+                        Text("picture")
+                        Text("picture")
+                    }
+                }
+            }
+
+            //hard difficulty
+            Row(){
+                Column(){
+                    Text("Hard")
+                    //pictures for hard difficulty
+                    Row(){
+                        Text("picture")
+                        Text("picture")
+                        Text("picture")
+                    }
+                }
+            }
+        }
     }
+
 }

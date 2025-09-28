924bd3c86868ba04118271595106620668f89c93
timoschwarzbach <timo.schwarzbach@outlook.com>
2025-09-28 13:17:07 +0300

    update compose navigation dependency declaration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 
     // Jetpack Compose navigation
-    implementation("androidx.navigation:navigation-compose:2.7.5")
-    
+    implementation(libs.androidx.navigation.compose)
+
     // persistance
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)

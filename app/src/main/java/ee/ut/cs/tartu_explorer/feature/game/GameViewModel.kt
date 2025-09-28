b2255f799faab5f4d84500a5b1eaaf5b0b4a5cc0
timoschwarzbach <timo.schwarzbach@outlook.com>
2025-09-28 21:17:27 +0300

    update game viewmodel

--- app/src/main/java/ee/ut/cs/tartu_explorer/feature/game/GameViewModel.kt
+++ app/src/main/java/ee/ut/cs/tartu_explorer/feature/game/GameViewModel.kt
@@ -3,25 +3,41 @@ package ee.ut.cs.tartu_explorer.feature.game
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import ee.ut.cs.tartu_explorer.core.data.local.entities.HintEntity
 import ee.ut.cs.tartu_explorer.core.data.repository.GameRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
 class GameViewModel(
     private val adventureId: Int,
     private val repository: GameRepository,
-): ViewModel() {
+) : ViewModel() {
     private val _state = MutableStateFlow(GameState())
+
+    private var _quests = repository
+        .getQuestsByAdventure(adventureId)
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
     @OptIn(ExperimentalCoroutinesApi::class)
-    private var _quests = _state.flatMapLatest { it -> repository.getQuestsByAdventure(adventureId) }
-    @OptIn(ExperimentalCoroutinesApi::class)
-//    private var _hints = _state.flatMapLatest { it -> dao.getHints(it.quests[it.currentQuest].id) }
-    private var _hints = _state.flatMapLatest { it -> repository.getHintsByQuest(1) }
+    private var _hints = _state
+        .combine(_quests) { state, quests ->
+            state.copy(
+                quests = quests
+            )
+        }
+        .flatMapLatest { it ->
+            if(_quests.value.isEmpty() || it.currentQuest > _quests.value.size) {
+                return@flatMapLatest flow { emptyList<HintEntity>() }
+            }
+            repository.getHintsByQuest(_quests.value[it.currentQuest].id)
+        }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     val state = combine(_state, _quests, _hints) { state: GameState, quests, hints ->
         state.copy(
             quests = quests,
@@ -30,19 +46,16 @@ class GameViewModel(
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), GameState())
 
     fun nextQuest() {
-        if(state.value.currentQuest < state.value.quests.size) {
-            state.value.copy(
-                currentHint = 1,
-                currentQuest = state.value.currentQuest+1
-            )
+        if (_state.value.currentQuest < state.value.quests.size) {
+            _state.update { it -> it.copy(currentQuest = it.currentQuest + 1, currentHint = 0) }
         }
     }
 
     fun requestNextHint() {
-        if(state.value.currentHint < state.value.hints.size) {
-            _state.update { it.copy(
-                currentHint = state.value.currentHint+1
-            ) }
+        if (_state.value.currentHint < state.value.hints.size) {
+            _state.update { it ->
+                it.copy(currentHint = _state.value.currentHint + 1)
+            }
         }
     }
 }

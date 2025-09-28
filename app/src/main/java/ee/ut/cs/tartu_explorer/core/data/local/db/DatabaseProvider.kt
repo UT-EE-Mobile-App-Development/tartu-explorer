9bd78e4de5c4c41b84dc80382a462ac1f4bc3027
timoschwarzbach <timo.schwarzbach@outlook.com>
2025-09-28 17:21:46 +0300

    add database provider

---
+++ app/src/main/java/ee/ut/cs/tartu_explorer/core/data/local/db/DatabaseProvider.kt
@@ -0,0 +1,21 @@
+package ee.ut.cs.tartu_explorer.core.data.local.db
+
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+    @Volatile
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+        return INSTANCE ?: synchronized(this) {
+            Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "tartuExplorer.db"
+            )
+                // .createFromAsset("exampleData.db")
+                .build().also { INSTANCE = it }
+        }
+    }
+}

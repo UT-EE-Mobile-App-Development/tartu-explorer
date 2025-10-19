5b0a1dcd156564f9ca8c6d1b781328ee7b797610
Philipp Rebeschieß <philipp.rebeschiess@uni-potsdam.de>
2025-10-19 23:52:13 +0300

    Update `QuestAttemptEntity` and `PlayerEntity` to use `Long` type for primary key IDs.

--- app/src/main/java/ee/ut/cs/tartu_explorer/core/data/local/entities/PlayerEntity.kt
+++ app/src/main/java/ee/ut/cs/tartu_explorer/core/data/local/entities/PlayerEntity.kt
@@ -8,9 +8,6 @@ import androidx.room.PrimaryKey
  *
  * @property id The primary key representing the player's unique identifier. Defaults to 1, assuming a single player scenario.
  * @property name The name of the player.
- * @property completedQuests The total number of quests completed by the player. Defaults to 0.
- * @property totalSteps The cumulative number of steps taken by the player across all quests. Defaults to 0.
- * @property hintsUsed The total number of hints utilized by the player. Defaults to 0.
  * @property experiencePoints The number of experience points earned by the player. Defaults to 0.
  */
 @Entity(tableName = "player")

cb2c1c06c0c9e87084c8a5b5533af4f9f51d815f
madis <puumadis@gmail.com>
2025-10-21 15:50:51 +0300

    Statistics screen data organised, commented

--- app/src/main/java/ee/ut/cs/tartu_explorer/feature/statistics/StatisticsScreen.kt
+++ app/src/main/java/ee/ut/cs/tartu_explorer/feature/statistics/StatisticsScreen.kt
@@ -4,8 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*

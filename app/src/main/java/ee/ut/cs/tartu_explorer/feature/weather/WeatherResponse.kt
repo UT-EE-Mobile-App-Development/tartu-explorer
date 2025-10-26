ad118879852e1bd031e2e3772916cbbbf0ffd29d
Rando Roosik <roosikrando@gmail.com>
2025-10-26 16:24:21 +0200

    Added weather API

---
+++ app/src/main/java/ee/ut/cs/tartu_explorer/feature/weather/WeatherResponse.kt
@@ -0,0 +1,16 @@
+package ee.ut.cs.tartu_explorer.feature.weather
+
+data class WeatherResponse(
+    val latitude: Double,
+    val longitude: Double,
+    val current_weather: CurrentWeather
+)
+
+data class CurrentWeather(
+    val temperature: Double,
+    val windspeed: Double,
+    val winddirection: Double,
+    val weathercode: Int,
+    val is_day: Int,
+    val time: String
+)
